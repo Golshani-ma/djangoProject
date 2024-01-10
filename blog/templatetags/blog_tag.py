13d73be49aa57ef0df9fe1ae5a87f798983e8159
Golshani-ma <golshani.ma@gmail.com>
2024-01-10 23:32:56 +0330

    Chapter 7, Exercise Latest_post_Bottom

--- blog/templatetags/blog_tag.py
+++ blog/templatetags/blog_tag.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post, Category
+from django.utils import timezone
 
 register = template.Library()
 
@@ -22,7 +23,7 @@ def popular_posts():
 
 
 @register.inclusion_tag('blog/blog-lastposts.html')
-def latest_post(num=4):
+def latest_post_side(num=4):
     posts = Post.objects.filter(status=1).order_by('-published_date')[:num]
     return {'posts': posts}
 
@@ -33,7 +34,17 @@ def blog_post_categories():
     categories = Category.objects.all()
     cat_dict = {}
     for cat_name in categories:
-
         cat_dict[cat_name.name] = posts.filter(category=cat_name).count()
 
-    return {'categories':cat_dict}
+    return {'categories': cat_dict}
+
+
+@register.inclusion_tag('website/website-latest-posts.html')
+def latest_post_bottom(num=6):
+    '''
+    Capter 7 Exersice Template Tag
+    '''
+    current_time = timezone.now()
+    posts = Post.objects.filter(status=1, published_date__lte=current_time).order_by('-published_date')[:num]
+
+    return {'posts': posts}
