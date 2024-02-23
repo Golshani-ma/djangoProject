47eb42e8390189b0f1d1a90c0609e1ec464eda0e
Golshani-ma <golshani.ma@gmail.com>
2024-02-23 14:02:14 +0330

    prod1.py

--- djangoProject/setting/prod.py
+++ djangoProject/setting/prod.py
@@ -1,3 +1,5 @@
+from django.conf.global_settings import CSRF_COOKIE_SECURE
+
 from djangoProject.settings import *
 
 SECRET_KEY = 'django-insecure-7lv*%$+)wvbaft82e2b&o@i3j@z$(@wrv7@$9t5kg&=3fw!5kd'
@@ -18,10 +20,9 @@ DATABASES = {
 
 
 STATIC_ROOT = BASE_DIR / 'static'
-#
-
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics"
-]
+CSRF_COOKIE_SECURE=True
+# STATICFILES_DIRS = [
+#     BASE_DIR / "statics"
+# ]
